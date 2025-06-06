9236ab112abd8af684a4f19d488c26aea4e08cdb
RedactedRice <RedactedRice@gmail.com>
2025-06-06 00:05:10 -0400

    Updating violent growth to two click. Still need to update tool tip

--- Treeherders/scripts/weapons/th_violentgrowth.lua
+++ Treeherders/scripts/weapons/th_violentgrowth.lua
@@ -19,15 +19,16 @@ Treeherders_ViolentGrowth = Skill:new
     Upgrades = 2,
     UpgradeCost = { 1, 2 },
 	
+	TwoClick = true,
+	
 	-- custom options
 	ForestDamageBounce = -2,
 	NonForestBounce = 2,
 	ForestGenBounce = forestUtils.floraformBounce,
 	
-	PushTarget = false,
 	SeekVek = true,
 	
-	ForestToExpand = 1,
+	ForestToExpand = 0,
 	SlowEnemy = false,
 	SlowEnemyAmount = 3,
 	MinEnemyMove = 1,
@@ -53,7 +54,7 @@ Weapon_Texts.Treeherders_ViolentGrowth_Upgrade2 = "+2 Expansion"
 Treeherders_ViolentGrowth_B = Treeherders_ViolentGrowth:new
 {
 	UpgradeDescription = "Expand the targeted forest two extra tiles",
-	ForestToExpand = 3,
+	ForestToExpand = 2,
 }
 
 Treeherders_ViolentGrowth_AB = Treeherders_ViolentGrowth_B:new
@@ -61,106 +62,65 @@ Treeherders_ViolentGrowth_AB = Treeherders_ViolentGrowth_B:new
 	SlowEnemy = true,
 }
 
+function Treeherders_ViolentGrowth:GetTargetArea(point)
+	local ret = PointList()
+	
+	for dir = DIR_START, DIR_END do
+		ret:push_back(point + DIR_VECTORS[dir])
+	end
+	return ret
+end
+
 function Treeherders_ViolentGrowth:GetSkillEffect(p1, p2)
 	local ret = SkillEffect()
 	local attackDir = GetDirection(p2 - p1)
-	
-	----- For the main target ------
-	local pushDir = nil
-	if self.PushTarget then
-		pushDir = attackDir
-	end
 		
 	--if it is a forest, cancel the target's attack
 	if forestUtils.isAForest(p2) then
-		ret:AddDamage(forestUtils:getSpaceDamageWithoutSettingFire(p2, self.Damage, pushDir, true, true))
+		ret:AddDamage(forestUtils:getSpaceDamageWithoutSettingFire(p2, self.Damage, nil, true, true))
 		forestUtils:addCancelEffect(p2, ret)
 		ret:AddBounce(p2, self.NonForestBounce)
 	
 	--if it can be floraformed, do so
 	elseif forestUtils.isSpaceFloraformable(p2) then
-		forestUtils:floraformSpace(ret, p2, self.Damage, pushDir, false, true)
+		forestUtils:floraformSpace(ret, p2, self.Damage, nil, false, true)
 		
 	--otherwise just damage it
 	else
 		ret:AddDamage(SpaceDamage(p2, self.Damage))
 		ret:AddBounce(p2, self.NonForestBounce)
 	end
+	return ret
+end
+
+function Treeherders_ViolentGrowth:GetSecondTargetArea(p1, p2)
+	local ret = PointList()
+	
+	local forestGroup = forestUtils:getGroupingOfSpaces(p2, forestUtils.isAForest)
+	for k, v in pairs(forestGroup.group) do
+		if Point(v) ~= p2 and forestUtils.isSpaceFloraformable(v) then
+			ret:push_back(Point(v))
+		end
+	end
+	for k, v in pairs(forestGroup.boardering) do
+		if Point(v) ~= p2 and forestUtils.isSpaceFloraformable(v) then
+			ret:push_back(Point(v))
+		end
+	end
+	return ret
+end
+
+function Treeherders_ViolentGrowth:GetFinalEffect(p1, p2, p3)
+	-- Start with the previous partial effect, add a pause and build on it
+	local ret = self:GetSkillEffect(p1, p2)
 	
 	--small break to make the animation and move make more sense
 	ret:AddDelay(0.4)
 	
-	
-	----- For expansion ------
-	
-	--pick tiles to expand to and damage if appropriate
-	--get tiles closest to enemies
-	local expansionFocus = p1
-	if self.SeekVek then
-		local vekPositions = {}
-		for _, v in pairs(extract_table(Board:GetPawns(TEAM_ANY))) do
-			if forestUtils.isAVek(Board:GetPawn(v)) then
-				local vPos = Board:GetPawnSpace(v)
-				-- exclude if the vek is the target or already is in a forest
-				if (vPos ~= p2) and not forestUtils.isAForest(vPos) then
-					vekPositions[forestUtils:getSpaceHash(vPos)] = vPos
-				end
-			end
-		end
-		
-		if forestUtils.arrayLength(vekPositions) > 0 then
-			expansionFocus = forestUtils:getClosestOfSpaces(p2, vekPositions)
-		end
-	end
-	
-	--Get all spaces in the grouping
-	local forestGroup = forestUtils:getGroupingOfSpaces(p2, forestUtils.isAForest)
-	--ensure the space we just formed is not in the boarding list - it will be in the group list
-	forestGroup.boardering[forestUtils:getSpaceHash(p2)] = nil
-	
-	local candidates = {}
-	for k, v in pairs(forestGroup.boardering) do
-		if forestUtils.isSpaceFloraformable(v) and v ~= p2 then
-			candidates[k] = v
-		end
-	end
-	
-	local newForests = {}
-	for i = 1, self.ForestToExpand do
-		if forestUtils.arrayLength(candidates) > 0 then
-			--get the nearest point, and remove it from the candidates
-			local expansion = forestUtils:getClosestOfSpaces(expansionFocus, candidates)
-			candidates[forestUtils:getSpaceHash(expansion)] = nil
-			newForests[forestUtils:getSpaceHash(expansion)] = expansion
-			
-			--floraform it
-			forestUtils:floraformSpace(ret, expansion, self.Damage, nil, true, true)
-		end
-	end
-	newForests[forestUtils:getSpaceHash(p2)] = p2
-	
-	
-	----- for pushing target -----
-	
-	--evaluate if we are pushing a fire unit onto a space we are floraforming because otherwise
-	--we will put out the enemy
-	if self.PushTarget then
-		local pawn = Board:GetPawn(p2)
-		
-		if pawn and pawn:IsFire() and not ret.effect:empty() then
-			for _, spaceDamage in pairs(extract_table(ret.effect)) do	
-				if spaceDamage.loc == p2 + DIR_VECTORS[pushDir] then
-					spaceDamage.iFire = EFFECT_NONE
-				end
-			end
-		end
-	end
-	
-	
-	----- For slowing enemies -----
-	
 	--any enemy in the forest, slow down temporarily if the powerup is enabled
 	if self.SlowEnemy then
+		local slowed = false
+		local forestGroup = forestUtils:getGroupingOfSpaces(p2, forestUtils.isAForest)
 		for _, v in pairs(forestGroup.group) do
 			local pawn = Board:GetPawn(v)
 			if pawn and pawn:IsEnemy() then
@@ -171,9 +131,52 @@ function Treeherders_ViolentGrowth:GetSkillEffect(p1, p2)
 				end 
 				
 				ret:AddScript([[Board:GetPawn(]]..pawn:GetId()..[[):AddMoveBonus(]]..slow..[[)]])
+				slowed = true
 			end
+		end
+		if slowed then
+			ret:AddDelay(0.2)
 		end
 	end
 	
+	if forestUtils.isSpaceFloraformable(p3) then
+		-- dont damage self
+		if p3 == p1 then
+			forestUtils:floraformSpace(ret, p3, 0, nil, false, true)
+		else
+			forestUtils:floraformSpace(ret, p3, self.Damage, nil, false, true)
+		end
+	end
+	
+	local numLeft = self.ForestToExpand
+	local selectedSpaced = {}
+	local attackDir = GetDirection(p2 - p1)
+	local dirPreferences = { (attackDir + 2) % 4, attackDir, (attackDir - 1) % 4, (attackDir + 1) % 4 }
+	
+	for _, dir in pairs(dirPreferences) do
+		if numLeft <= 0 then
+			break
+		end
+		local p = p3 + DIR_VECTORS[dir]
+		if Board:IsValid(p) and p ~= p2 and not forestUtils.isAForest(p) and forestUtils.isSpaceFloraformable(p) and not selectedSpaced[forestUtils:getSpaceHash(p)] then
+			selectedSpaced[forestUtils:getSpaceHash(p)] = p
+			numLeft = numLeft - 1
+		end 
+	end
+	
+	for _, dir in pairs(dirPreferences) do
+		if numLeft <= 0 then
+			break
+		end
+		local p = p2 + DIR_VECTORS[dir]
+		if Board:IsValid(p) and p ~= p3 and not forestUtils.isAForest(p) and forestUtils.isSpaceFloraformable(p) and not selectedSpaced[forestUtils:getSpaceHash(p)] then
+			selectedSpaced[forestUtils:getSpaceHash(p)] = p
+			numLeft = numLeft - 1
+		end 
+	end
+	
+	for _, v in pairs(selectedSpaced) do
+		forestUtils:floraformSpace(ret, v, self.Damage, nil, true, true)
+	end
 	return ret
 end
--- readme.txt
+++ readme.txt
@@ -6,11 +6,12 @@ ItB Discord: 	Das Keifer
 Email:		RedactedRice@gmail.com
 
 Latest release:
-0.10.1 - 5/30/2025 - compatible with:
+1.0.0 - XX/XX/2025 - compatible with:
 * ItB AE 	1.2.93
 * ModLoader 	2.9.4
-* ModLoaderExt	1.21 (needs updates for 1.23 APIs)
+* ModLoaderExt	1.21
 * memedit	1.2.0
 
 Previous releases:
+* 0.10.1 - 5/30/2025 - First AE update
 * 0.9.0 - 1/4/19 - ItB pre-AE
